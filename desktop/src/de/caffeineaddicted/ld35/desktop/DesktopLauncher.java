b8481d748f9656af473783af9b024579b1d3dfc5
Malte Heinzelmann <malte@hnzlmnn.de>
2016-04-16 01:05:50 +0200

    Project initialized

---
+++ desktop/src/de/caffeineaddicted/ld35/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.caffeineaddicted.ld35.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.caffeineaddicted.ld35.CoffeeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoffeeGame(), config);
+	}
+}
